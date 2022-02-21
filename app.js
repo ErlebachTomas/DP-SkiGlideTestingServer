f0e6345c94519b8e29e2a88dd9ab01dbcef116c9
Tomas_Erlebach <t.erleb@seznam.cz>
2022-02-21 17:20:46 +0100

    procfile

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-'use strict';
-var debug = require('debug')('my express app');
+﻿'use strict';
+const dotenv = require('dotenv').config();
+const debug = require('debug')('myApp');
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -25,7 +27,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', api);
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,19 @@
-'use strict';
+﻿'use strict';
 var express = require('express');
 var router = express.Router();
 
 /* GET */
 router.get('/', function (req, res) {
     res.send(req.body);
+});
+/* POST */
+router.post('/', function (req, res) {
+    res.send({ "req.body": req.body }); //pro kontrolu
+});
+
+/* todo remove test */
+router.get('/data', function (req, res) {
+    res.json({ data: 'test' });
 });
 
 module.exports = router;
